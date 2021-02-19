292d61709602f9b56d7e4bd546b6f7163b3d4d1b
Aditya Togani <adityatogani94@gmail.com>
2021-02-18 19:34:58 -0800

    Time: 96 ms (59.31%), Space: 26.2 MB (13.85%) - LeetHub

---
+++ delete-node-in-a-linked-list/delete-node-in-a-linked-list.cs
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public void DeleteNode(ListNode node) {
+        ListNode next = node.next;
+        node.val = next.val;
+        node.next = next.next;
+    }
+}
