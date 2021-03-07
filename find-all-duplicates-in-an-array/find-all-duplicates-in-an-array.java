10bbba5424db6bca36050c5ea6dc4296366ee5f2
Aditya Togani <adityatogani94@gmail.com>
2021-03-06 17:25:33 -0800

    Time: 6 ms (55.80%), Space: 48.2 MB (50.36%) - LeetHub

---
+++ find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.java
@@ -0,0 +1,16 @@
+
+//Mark the num with a negative sign if already visited
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+
+        for (int num : nums) {
+            if (nums[Math.abs(num) - 1] < 0) { // seen before
+                ans.add(Math.abs(num));
+            }
+            nums[Math.abs(num) - 1] *= -1;
+        }
+
+        return ans;
+    }
+}
